8db3431ebe066d0a2f8933981718e978336fdcfd
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-03-04 22:00:49 -0500

    commit tree 120a074151cbcdde8104ce48c16f55c14dcd6ae5

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+    "Hello, World!"
+end
+
+get '/name' do
+    "My name is Luis Castillo"
+end
+
+get '/hometown' do
+    "My hometown is Dominican Republic"
+end
+
+get '/favorite-song' do
+    "My favorite song is Reggaeton"
+end
+
 end
